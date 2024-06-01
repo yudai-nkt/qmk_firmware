c3691a102332722cdf0f5232906c879f5e4edd3c
Mark Stosberg <mark@stosberg.com>
2024-06-01 16:14:34 -0400

    chore: resolve issue with case of OLED driver name

--- keyboards/aki27/cocot46plus/rules.mk
+++ keyboards/aki27/cocot46plus/rules.mk
@@ -27,7 +27,7 @@ POINTING_DEVICE_ENABLE = yes
 POINTING_DEVICE_DRIVER = adns5050
 
 OLED_ENABLE     = yes
-OLED_DRIVER     = SSD1306
+OLED_DRIVER     = ssd1306
 
 # Disable unnecessary functions to reduce firmware size
 SPACE_CADET_ENABLE = no
