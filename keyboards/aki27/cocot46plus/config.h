8ec47dbe2e6e4fddaa55068699d4e29efd14fe30
Yudai NAKATA <yudai.nkt@gmail.com>
2024-07-21 02:25:32 +0900

    fix: migrate MCU converter config to keyboard.json

--- keyboards/aki27/cocot46plus/config.h
+++ keyboards/aki27/cocot46plus/config.h
@@ -18,17 +18,13 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #pragma once
 
 // These do not seem to have keyboard.json support yet.
-#define ADNS5050_SCLK_PIN         B2
-#define ADNS5050_SDIO_PIN         B4
-#define ADNS5050_CS_PIN           B5
+#define ADNS5050_SCLK_PIN         GP23
+#define ADNS5050_SDIO_PIN         GP8
+#define ADNS5050_CS_PIN           GP9
 
 /* key matrix size */
 #define MATRIX_ROWS 10
 #define MATRIX_COLS 6
-
-// wiring of each half
-#define MATRIX_ROW_PINS { D4, C6, D7, E6, NO_PIN }
-#define MATRIX_COL_PINS { F4, F5, F6, F7, B1, B3 }
 
 #define POINTING_DEVICE_ROTATION_180
 #define OLED_FONT_H "keyboards/aki27/cocot46plus/glcdfont.c"
